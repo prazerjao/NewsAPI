7605ae0ac0a2feb6f57c1fd9d8101876a4f5f62c
João Victor <73292345+prazerjao@users.noreply.github.com>
2022-08-17 14:16:12 -0300

    Initial Commit

---
+++ TestCheckList/TestCheckListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCheckListApp.swift
+//  TestCheckList
+//
+//  Created by João Victor  on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TestCheckListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
